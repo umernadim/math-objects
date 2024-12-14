1bb258dca2c54266642c5e2e98971c4eb04f826d
umernadim <itsumernadeem@gmail.com>
2024-12-14 08:31:38 -0800

    files added

---
+++ script.js
@@ -0,0 +1,53 @@
+// for converting decimal value into integar but goes towards bigger value
+// let a = Math.ceil(2.5)
+// document.write(a)
+
+// for converting decimal value into integar but goes towards smaller value
+// let a = Math.floor(2.5)
+// let a = Math.floor(0.5)
+// document.write(a)
+
+// for converting decimal value into integar 
+// let a = Math.round(2.2)
+// let a = Math.round(5.67)
+// document.write(a)
+
+// it only gives the value in integar form, Does not give the value which is after point
+// let a = Math.trunc(2.2)
+// let a = Math.trunc(5.67)
+// document.write(a)
+
+// for finding maximum value 
+// let a = Math.max(5, 4, 7 ,10 ,3)
+// let a = Math.max(-2, 5, -3, 0, -7)
+// document.write(a)
+
+// for finding minimum value 
+// let a = Math.min(5, 4, 7 ,10 ,3)
+// let a = Math.min(-2, 5, -3, 0, -7)
+// document.write(a)
+
+// to know about square root 
+// let a = Math.sqrt(16)
+// let a = Math.sqrt(81)
+// document.write(a)
+
+// to know about cubic root 
+// let a = Math.cbrt(1000)
+// let a = Math.cbrt(8)
+// document.write(a)
+
+// it generates random value between 0 to 1
+// let a = Math.random()
+// if i want value between 1 to 10 then....
+// let a = Math.floor(Math.random() *10) +1
+// document.write(a)
+
+// it gives the absolute posiive value rather negative or positive
+// let a = Math.abs(5.67)
+// let a = Math.abs(-5.67)
+// document.write(a)
+
+// for the value of Pi 
+// let a = Math.PI
+// document.write(a)
